b7728fff13ce9bb7f2ef9ef396f85d4889dae015
Taylor Pool <taylorpool.27@gmail.com>
2020-12-12 20:57:25 -0700

    Added verification for create_message of CFG_VALGET type

--- test/test_ubx_message.cpp
+++ test/test_ubx_message.cpp
@@ -27,3 +27,62 @@ TEST(CreateUBXMessage, ACK_ACK_Checksum_B)
     UBX_message_t message = create_message(kCLASS_ACK, kACK_ACK, 2, payload);
     ASSERT_EQ(message.get_checksum_b(), 195);
 }
+
+class CreateCFGVALGETMessage: public ::testing::Test
+{
+    protected:
+        UBX_message_t cfg_message;
+        uint16_t my_position = 0x0010;
+        uint32_t my_key_id = CFG_VALGET_t::kMSGOUT_SFRBX;
+
+        void SetUp()
+        {
+            union
+            {
+                CFG_VALGET_t::request_t request;
+                uint8_t payload[8];
+            };
+
+            request.version = CFG_VALGET_t::kREQUEST;
+            request.layer = CFG_VALGET_t::kRAM;
+            request.position = my_position;
+            request.cfgDataKey.keyID = my_key_id;
+
+            cfg_message = create_message(kCLASS_CFG, kCFG_VALGET, 8, payload);
+        }
+};
+
+TEST_F(CreateCFGVALGETMessage, TestkCLASSCFG)
+{
+    ASSERT_EQ(cfg_message.message_class, kCLASS_CFG);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestkCFG_VALGET)
+{
+    ASSERT_EQ(cfg_message.message_id, kCFG_VALGET);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestLength)
+{
+    ASSERT_EQ(cfg_message.payload_length, 8);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestVersion)
+{
+    ASSERT_EQ(cfg_message.payload.CFG_VALGET.version, CFG_VALGET_t::kREQUEST);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestLayer)
+{
+    ASSERT_EQ(cfg_message.payload.CFG_VALGET.layer, CFG_VALGET_t::kRAM);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestPosition)
+{
+    ASSERT_EQ(cfg_message.payload.CFG_VALGET.position, my_position);
+}
+
+TEST_F(CreateCFGVALGETMessage, TestKeyID)
+{
+    ASSERT_EQ(cfg_message.payload.CFG_VALGET.cfgDataKey.keyID, my_key_id);
+}
