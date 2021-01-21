0d97ada8d1dba835068b06ef967b4a5ba0314bf1
Taylor Pool <taylorpool.27@gmail.com>
2021-01-20 19:27:59 -0700

    Added support for listening to GPS constellations

--- src/executables/satellite_listener.cpp
+++ src/executables/satellite_listener.cpp
@@ -12,6 +12,16 @@ int main(int argc, char* argv[])
     }
 
     std::shared_ptr<ublox::Ublox> my_ublox = std::make_shared<ublox::Ublox>(serial_port);
-    std::cout<<ublox::configure::configure_moving_base(my_ublox)<<std::endl;
+    ublox::configure::configure_moving_base(my_ublox);
+    ublox::configure::val_set(my_ublox, ublox::ubx::CFG_VALSET_t::kSIGNAL_GPS, static_cast<uint8_t>(1));
+    ublox::configure::val_set(my_ublox, ublox::ubx::CFG_VALSET_t::kSIGNAL_GPS_L1, static_cast<uint8_t>(1));
+    ublox::configure::val_set(my_ublox, ublox::ubx::CFG_VALSET_t::kSIGNAL_GPS_L2, static_cast<uint8_t>(1));
+    gnss::NavParser sfrbx_parser;
+    my_ublox->parser.register_callback(ublox::ubx::kCLASS_RXM, ublox::ubx::kRXM_SFRBX, [&sfrbx_parser](const ublox::ubx::UBX_message_t &ubx_msg)
+    {
+        sfrbx_parser.parse_sfrbx(ubx_msg);
+    });
+
+    while(true);
     return 0;
 }
