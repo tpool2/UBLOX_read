523a9d87c88e166d639b1dc02f8811f649bc9e0b
Taylor Pool <taylorpool.27@gmail.com>
2021-02-17 16:09:23 -0700

    Cleaned up testing for GPS LNAV

--- GNSS/test/test_gps_lnav.cpp
+++ GNSS/test/test_gps_lnav.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "GNSS/gps_lnav.hpp"
 #include <iostream>
+#include <tuple>
 
 uint32_t sat_03[]{  0X22c05d10, 0X28ccaa9c,
                     0Xc82c7d6,  0X8dba513c,
@@ -34,93 +35,61 @@ uint32_t sat_27[]{  0x22c05d10, 0x28ccaa9c,
 
 class UBLOX_LNAV: public ::testing::TestWithParam<uint32_t*>
 {
-    
+    protected:
+        uint32_t* words;
+        void SetUp() override
+        {
+            words = GetParam();
+        }
 };
 
 TEST_P(UBLOX_LNAV, Preamble_Check)
 {
-    ASSERT_EQ(bit_utils::get_msb_bits<uint8_t>(GetParam(), 2, 8), gnss::gps::kPreamble);
+    ASSERT_EQ(bit_utils::get_msb_bits<uint8_t>(words, 2, 8), gnss::gps::kPreamble);
 }
 
-TEST_P(UBLOX_LNAV, TLM_Parity_Check)
-{
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[0], 0, 0));
-}
-
-TEST_P(UBLOX_LNAV, HOW_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[0], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[0], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[1], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Third_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[1], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[1], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[2], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Fourth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[2], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[2], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[3], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Fifth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[3], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[3], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[4], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Sixth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[4], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[4], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[5], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Seventh_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[5], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[5], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[6], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Eighth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[6], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[6], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[7], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Ninth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[7], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[7], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[8], D_29, D_30));
-}
-
-TEST_P(UBLOX_LNAV, Tenth_Word_Parity_Check)
-{
-    uint32_t* words = GetParam();
-    bool D_29 = bit_utils::get_msb_bits<bool>(&words[8], 30, 1);
-    bool D_30 = bit_utils::get_msb_bits<bool>(&words[8], 31, 1);
-    ASSERT_TRUE(gnss::gps::lnav::check_parity(GetParam()[9], D_29, D_30));
-}
-
-INSTANTIATE_TEST_SUITE_P(Sat_22, UBLOX_LNAV,
+INSTANTIATE_TEST_SUITE_P(NavData, UBLOX_LNAV,
 testing::Values
 (
     sat_03, sat_07, sat_22, sat_26, sat_27
+));
+
+class UBLOX_LNAV_With_Word_Index: public ::testing::TestWithParam<std::tuple<uint32_t*, int>>
+{
+    protected:
+        uint32_t* words;
+        int word_index;
+        void SetUp() override
+        {
+            auto params = GetParam();
+            words = std::get<0>(params);
+            word_index = std::get<1>(params);
+        }
+};
+
+TEST_P(UBLOX_LNAV_With_Word_Index, Parity_Check)
+{
+    bool D_29, D_30;
+    if(word_index > 0)
+    {
+        int previous_word_index = word_index-1;
+        D_29 = bit_utils::get_msb_bits<bool>(&words[previous_word_index], 30, 1);
+        D_30 = bit_utils::get_msb_bits<bool>(&words[previous_word_index], 31, 1);
+    }
+    else
+    {
+        D_29 = 0;
+        D_30 = 0;
+    }
+    ASSERT_TRUE(gnss::gps::lnav::check_parity(words[word_index], D_29, D_30));
+}
+
+INSTANTIATE_TEST_SUITE_P(NavData, UBLOX_LNAV_With_Word_Index,
+testing::Combine
+(
+    testing::Values
+    (
+        sat_03, sat_07, sat_22, sat_26, sat_27
+    ),
+    testing::Range(0, 10)
 ));
