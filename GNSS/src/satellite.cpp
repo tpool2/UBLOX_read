fa033b3dee044e665324fdd5e19697a8a030c214
Taylor Pool <taylorpool.27@gmail.com>
2021-03-01 18:26:44 -0700

    Started getting a full subframe

--- GNSS/src/satellite.cpp
+++ GNSS/src/satellite.cpp
@@ -32,6 +32,15 @@ void SatelliteDatabase::update(const ublox::ubx::UBX_message_t& message)
                 {
                     constellation_map[kGPS][sfrbx.svId] = std::make_shared<gps::GPS_Satellite>(sfrbx.svId);
                 }
+                std::cout<<"SV ID: "<<uint16_t(sfrbx.svId)<<std::endl;
+                std::cout<<"Subframe ID: " << uint16_t(gps::lnav::get_bits<uint8_t>(&sfrbx.dwrd[1], 19, 3)) << std::endl;
+                if(sfrbx.svId == 4)
+                {
+                    for(int i = 0; i < gps::kSubframeLength; ++i)
+                    {
+                        std::cout << std::hex << sfrbx.dwrd[i]<<std::dec<<std::endl;
+                    }
+                }
                 constellation_map[kGPS][sfrbx.svId]->update(sfrbx.dwrd);
             }
             else if(bit_utils::get_msb_bits<uint8_t>(sfrbx.dwrd, 0, 8) == gps::kPreamble)
@@ -51,7 +60,6 @@ namespace gps
 {
     bool GPS_Satellite::update(uint32_t* words)
     {
-        std::cout<<uint16_t(lnav::get_bits<uint8_t>(&words[1], 19, 3)) << std::endl;
         return true;
     }
 }
